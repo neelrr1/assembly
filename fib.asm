5e8b839c575db2c04775c25f458f1fc3d4ac5805
Neel Raja <neelrr1@gmail.com>
2024-01-26 02:30:21 -0800

    more bits for fib!

--- fib.asm
+++ fib.asm
@@ -14,6 +14,35 @@ STDOUT = 1
 
 ENDL = 10
 
+; subroutine to print the number stored in rax to stdout
+print_number:
+    ; count how many digits are in the input and write remainders to buf
+    xor rbx, rbx ; store loop iterations in rbx
+
+r:
+    ; divide by 10
+    mov rcx, 10
+    xor rdx, rdx
+    div rcx
+
+    add rdx, 48 ; convert remainder to ascii
+    lea rsi, [buf + buf_len - 1]
+    sub rsi, rbx
+    mov byte [rsi], dl ; store character in buffer
+    inc rbx ; increment loop counter
+    cmp rax, 0 ; if # is not 0, repeat
+    jne r
+
+    ; print the answer
+    mov rax, SYSCALL_WRITE
+    mov rdi, STDOUT
+    lea rsi, [buf + buf_len]
+    sub rsi, rbx
+    mov rdx, rbx
+    syscall
+
+    ret
+
 print_newline:
     mov rax, SYSCALL_WRITE
     mov rdi, STDOUT
@@ -25,8 +54,10 @@ print_newline:
 
 main:
     ; Read from stdin in a loop until we read a newline
-    mov r8, 0 ; r8 will track how many loop iterations to do
+    xor r8, r8 ; r8 will track the input
+    mov r10, 10
     
+get_input:
     ; read(STDIN, &k, 1)
     mov rax, SYSCALL_READ
     mov rdi, STDIN
@@ -34,35 +65,45 @@ main:
     mov rdx, 1
     syscall
 
-    ; r8 is loop counter, r9 is prev prev, r10 is prev
-    mov r8b, [k]
-    sub r8b, 48 ; convert ASCII digit to real digit
+    ; break if we read a newline
+    cmp byte [k], 10
+    je init
+
+    ; r8 is the input number so far, r9 is the new digit we are trying to insert
+    movzx r9, byte [k]
+    sub r9, 48 ; convert ASCII digit to real digit
     
-    mov r9b, 0
-    mov r10b, 1
+    ; multiply r8 by 10
+    mov r10, r8
+    shl r8, 3
+    add r8, r10
+    add r8, r10
 
-loup:
-    mov byte[k], r10b
-    add byte[k], 48
-    
-    mov r11b, r10b
-    add r11b, r9b
+    ; insert the newly read digit
+    add r8, r9
+    jmp get_input
+init:
+    mov r9, 0
+    mov r10, 1
 
-    mov r9b, r10b
-    mov r10b, r11b
+fib:
+    ; calculate next number in sequence
+    mov r11, r10
+    add r11, r9
+    ; set previous numbers
+    mov r9, r10
+    mov r10, r11
 
-    mov rax, SYSCALL_WRITE
-    mov rdi, STDOUT
-    lea rsi, [k]
-    mov rdx, k_len
-    syscall
-
-    dec r8b
-    cmp r8b, 0
-    jnz loup
-
+    ; print number stored in r10
+    mov rax, r10
+    call print_number
     call print_newline
 
+    dec r8
+    cmp r8, 0
+    jnz fib
+
+exit:
     ; Exit with EXIT_CODE
     mov rax, SYSCALL_EXIT
     mov rdi, EXIT_CODE
@@ -70,7 +111,8 @@ loup:
 
 segment readable writeable
 k: rb 1
-db ' '
 k_len = $ - k
+buf: rb 20 ; longest 64 bit number is 20 digits
+buf_len = $ - buf
 
 newline: db 10
